83afb240be21bc6ec683ce3c0dc943c94d618a86
PiotrKaszuba <piotrkaszuba1996@gmail.com>
2025-09-13 06:47:55 +0200

    mock_test takes args and passes to test, better print separator after mock_test

--- tests/helpers/mock_test.lua
+++ tests/helpers/mock_test.lua
@@ -1,7 +1,8 @@
 local mock = require("tests.mock.factorio_runtime")
 
-return function(fn)
-    fn()
+return function(fn, args)
+    fn(args)
     print(string.rep("-", 30))
     mock.performance.report()
+    print(string.rep("-", 30))
 end
